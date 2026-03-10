256bb48326dd14d1acd8078ed2797d1612a8c598
alicedetter <detteralice@gmail.com>
2026-03-10 13:35:56 +0100

    h

--- index.php
+++ index.php
@@ -7,12 +7,21 @@
 </head>
 <body>
     <?php
-        $siffror = ["ett", "två", "tre", "fyra", "fem", "sex", "sju", "åtta", "nio", "tio"];
-        $counter = count($siffror);
-        for($x=0; $x<$counter; $x++){
-            echo "rad ", $siffror[$x], "<br>";
-        }
-    ?>
-    
+    if(isset($_GET['get_btn'])): ?>
+        <form action="index.php" method="POST">
+            <input type="text" name="text" value="<?=$_GET['text']?>">
+            <input type="submit" name="post_btn" value="skicka">
+        </form>
+    <?php elseif(isset($_POST['text'])): ?>
+        <form action="index.php" method="GET">
+            <input type="text" name="text" value="<?=$_POST['text']?>">
+            <input type="submit" name="get_btn" value="skicka">
+        </form>
+    <?php else: ?>
+        <form action="index.php" method="GET">
+            <input type="text" name="text" value="">
+            <input type="submit" name="get_btn" value="skicka">
+        </form>
+    <?php endif ?>
 </body>
 </html>
